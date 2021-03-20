314e9d9ea7ef8587550378ca8647c67dc25da204
Hien Luong <hyhien@gmail.com>
2021-03-20 17:06:14 +0700

    Code day 01

---
+++ Day01_Intro/Day01_Intro/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Day01_Intro.Models;
+
+namespace Day01_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // host/Home/XoSo
+        public int XoSo()
+        {
+            return new Random().Next();
+        }
+
+        // host/Home/Chao
+        // host/Home/Chao?ten=Nhất Nghệ
+        public string Chao(string ten = "")
+        {
+            return $"Chào bạn {ten}";
+        }
+
+        public IActionResult Privacy()
+        {
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day01_Intro/Day01_Intro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Day01_Intro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
