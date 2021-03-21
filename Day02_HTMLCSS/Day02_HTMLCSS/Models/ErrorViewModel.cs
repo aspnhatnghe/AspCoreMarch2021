9d197687a403864d8106a71ce6696875eca57866
Hien Luong <hyhien@gmail.com>
2021-03-21 09:48:39 +0700

    Day02 - HTML Basic

---
+++ Day02_HTMLCSS/Day02_HTMLCSS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Day02_HTMLCSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
