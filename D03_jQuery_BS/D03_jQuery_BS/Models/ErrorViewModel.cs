b6108760d95bcb3028f2e8319c7ac3b7a453f24a
Hien <hyhien@gmail.com>
2021-03-27 15:41:05 +0700

    Ban co

---
+++ D03_jQuery_BS/D03_jQuery_BS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D03_jQuery_BS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
