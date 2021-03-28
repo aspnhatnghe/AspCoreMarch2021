a1f4916a1162e5967109412ee2e7c6ef20ef78fd
Hien <hyhien@gmail.com>
2021-03-28 08:10:48 +0700

    Init day04

---
+++ D04_BootStrap/D04_BootStrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D04_BootStrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
