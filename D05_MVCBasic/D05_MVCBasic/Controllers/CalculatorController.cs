257d0351284c042db831b0ba11c0b7656c84325d
Hien <aspcorenn@gmail.com>
2021-04-03 17:25:37 +0700

    Done 05 - MVC Basic

---
+++ D05_MVCBasic/D05_MVCBasic/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D05_MVCBasic.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate(double SoHang01, string PhepToan, double SoHang02)
+        {
+            var ketQua = 0.0;
+            switch (PhepToan)
+            {
+                case "+": ketQua = SoHang01 + SoHang02; break;
+                case "-": ketQua = SoHang01 - SoHang02; break;
+                case "*": ketQua = SoHang01 * SoHang02; break;
+                case "/": ketQua = SoHang01 / SoHang02; break;
+                case "%": ketQua = SoHang01 % SoHang02; break;
+                case "^": ketQua = Math.Pow(SoHang01, SoHang02); break;
+            }
+            //return Content($"{SoHang01} {PhepToan} {SoHang02} = {ketQua}");
+
+            ViewBag.A = SoHang01;
+            ViewBag.B = SoHang02;
+            ViewBag.KQ = ketQua;
+            ViewBag.PT = PhepToan;
+            return View("Index");
+            //return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
---
+++ D05_MVCBasic/D05_MVCBasic/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using D05_MVCBasic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D05_MVCBasic.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            var sp = new Product
+            {
+                ProductId = 1, Name = "Beer", Price = 1.3
+            };
+
+            return View(sp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            return View();
+        }
+    }
+}
