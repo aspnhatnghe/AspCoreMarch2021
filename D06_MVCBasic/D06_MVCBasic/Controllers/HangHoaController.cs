ef7774037a12682107245628cb00ee579cbae6f7
Hien <aspcorenn@gmail.com>
2021-04-04 08:34:41 +0700

    D06 _ MVC

---
+++ D06_MVCBasic/D06_MVCBasic/Controllers/HangHoaController.cs
@@ -0,0 +1,28 @@
+﻿using D06_MVCBasic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace D06_MVCBasic.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        static List<HangHoa> HangHoas = new List<HangHoa>();
+
+        public HangHoaController()
+        {
+            HangHoas.Add(new HangHoa { 
+                MaHh = 1, TenHh = "Wakeup 247",
+                DonGia = 7000, SoLuong = 101, ConBan = true
+            });
+            HangHoas.Add(new HangHoa { 
+                MaHh = 2, TenHh = "7Up Revice",
+                DonGia = 7500, SoLuong = 81, ConBan = true
+            });
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ D06_MVCBasic/D06_MVCBasic/Models/HangHoa.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D06_MVCBasic.Models
+{
+    public class HangHoa
+    {
+        [Display(Name = "Mã hàng hóa")]
+        public int MaHh { get; set; }
+
+        [Display(Name = "Tên hàng hóa")]
+        public string TenHh { get; set; }
+
+        [Display(Name = "Đơn giá")]
+        public double DonGia { get; set; }
+
+        [Display(Name = "Số lượng")]
+        public int SoLuong { get; set; }
+
+        [Display(Name = "Đang bán")]
+        public bool ConBan { get; set; }
+    }
+}
