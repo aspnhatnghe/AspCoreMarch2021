f8fa1e250a7300868aaa103658099e8f5644d585
Hien <aspcorenn@gmail.com>
2021-04-10 14:29:21 +0700

    Lab-6 - Student lab - save txt

---
+++ Day07_MVC/Day07_MVC/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Day07_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Day07_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LuuXuongFile(Student sinhvien, string Save)
+        {
+            if (Save == "Lưu TXT")
+            {
+                var chuoi = new string[] {
+                    "Mã SV: " + sinhvien.StudentId,
+                    "Họ tên SV: " + sinhvien.StudentName,
+                    "Điểm: " + sinhvien.Diem,
+                    "Xếp loại: " + sinhvien.XepLoai
+                };
+
+                var textFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "studentinfo.txt");
+                System.IO.File.WriteAllLines(textFilePath, chuoi);
+            }
+
+            return View("Index");
+        }
+    }
+}
