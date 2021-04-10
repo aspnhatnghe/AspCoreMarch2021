82c57108d84d5523b8246f060dee9035154da598
Hien <aspcorenn@gmail.com>
2021-04-10 13:51:42 +0700

    Day07 Init

---
+++ Day07_MVC/Day07_MVC/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Day07_MVC.Models
+{
+    public class Student
+    {
+        [Display(Name ="Mã sinh viên")]
+        public int StudentId { get; set; }
+        [Display(Name = "Họ tên")]
+        public string StudentName { get; set; }
+        [Display(Name = "Điểm")]
+        public double Diem { get; set; }
+        [Display(Name = "Xếp loại")]
+        public string XepLoai
+        {
+            get
+            {
+                var xeploai = "Yếu";
+                if(Diem >= 9) { xeploai = "Xuất sắc"; }
+                else if (Diem >= 8.5) { xeploai = "Giỏi"; }
+                else if (Diem >= 7) { xeploai = "Khá"; }
+                else if (Diem >= 5) { xeploai = "Trung bình"; }
+                return xeploai;
+            }
+        }
+    }
+}
