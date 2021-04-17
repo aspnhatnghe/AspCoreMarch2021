0503a51f81b08d449c4a389dc793a3771c222692
Hien <aspcorenn@gmail.com>
2021-04-17 17:07:57 +0700

    Layout

---
+++ D08_Validation/D08_Validation/Controllers/LayoutController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using D08_Validation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D08_Validation.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MenuPhai()
+        {
+            return View();
+        }
+
+        public IActionResult NoTemplate()
+        {
+            return View();
+        }
+
+        public IActionResult ChildLayout()
+        {
+            return View();
+        }
+
+        public IActionResult DemoPartial()
+        {
+            return PartialView("_DanhMuc");
+        }
+        
+        public IActionResult DemoPartialWithData()
+        {
+            var data = new List<Category> {
+        new Category{ CategoryId = 1, CategoryName = "Điện máy"},
+        new Category{ CategoryId = 2, CategoryName = "Điện tử"},
+        new Category{ CategoryId = 3, CategoryName = "Điện lạnh"},
+        new Category{ CategoryId = 4, CategoryName = "Điện gia dụng"}
+    };
+            return PartialView("_DanhMuc", data);
+        }
+    }
+}
---
+++ D08_Validation/D08_Validation/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿
+namespace D08_Validation.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
