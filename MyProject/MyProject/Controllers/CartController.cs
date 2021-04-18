cf7996b6e6ae205c14bdd2b4842aabf955c0d3ae
Hien <aspcorenn@gmail.com>
2021-04-18 11:46:43 +0700

    Done layout

---
+++ MyProject/MyProject/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/Checkout")]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyProject/MyProject/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
