18c57260641267b12b5ba6cb1a07444a05d52789
Hien <aspcorenn@gmail.com>
2021-04-25 11:40:54 +0700

    Demo ADO.NET

--- D12_ADONET/D12_ADONET/Controllers/DemoController.cs
+++ D12_ADONET/D12_ADONET/Controllers/DemoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPCore.ADONETLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 namespace D12_ADONET.Controllers
@@ -6,16 +8,44 @@ namespace D12_ADONET.Controllers
     public class DemoController : Controller
     {
         string chuoiKetNoi = @"Data Source=.;Initial Catalog=eStore20;Integrated Security=True";
-        
+
         public IActionResult LayLoai()
         {
             var sql = "SELECT * FROM Loai ORDER BY TenLoai";
             SqlConnection connection = new SqlConnection(chuoiKetNoi);
             SqlDataAdapter da = new SqlDataAdapter(sql, connection);
             DataTable dtLoai = new DataTable();
-            da.Fill(dtLoai);            
-            
+            da.Fill(dtLoai);
+
             return View(dtLoai);
+        }
+
+        public IActionResult GetLoai()
+        {
+            var sql = "SELECT * FROM Loai ORDER BY TenLoai";
+            //DataTable dtLoai = DataProvider.TruyVan_LayDuLieu(sql);
+
+            var dtLoai = DataProvider.SelectData(sql, CommandType.Text, null);
+
+            return View("LayLoai", dtLoai);
+        }
+
+        public string ThemLoai(string Loai = "Bia")
+        {
+            try
+            {
+                var sqlInsert = $"INSERT INTO LOAI(TenLoai) VALUES(N'{Loai}') ";
+                SqlConnection con = new SqlConnection(chuoiKetNoi);
+                SqlCommand cmd = new SqlCommand(sqlInsert, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                return $"Thêm loại {Loai} thành công";
+            }
+            catch (Exception ex)
+            {
+                return $"Lỗi: {ex.Message}";
+            }
         }
     }
 }
