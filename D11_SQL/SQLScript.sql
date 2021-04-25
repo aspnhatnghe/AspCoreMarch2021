9e137b781d7cd26b129d27033003726adcf038d3
Hien <aspcorenn@gmail.com>
2021-04-25 09:23:26 +0700

    Demo SQL

---
+++ D11_SQL/SQLScript.sql
@@ -0,0 +1,93 @@
+﻿USE eStore20
+GO
+
+--Thêm mới loại
+INSERT INTO Loai(Hinh, MoTa, TenLoai)
+	VALUES (NULL, N'Bàn ghế', N'Bàn ghế')
+
+INSERT INTO Loai(TenLoai, MoTa)
+	VALUES (N'Điện máy', N'Các mặt hàng điện máy')
+
+SELECT * FROM Loai
+
+--Thêm Hàng hóa
+INSERT INTO HangHoa(TenHH, DonGia, MoTaDonVi, MaLoai,
+	MaNCC, MoTa) VALUES(N'Tủ lạnh Panasonic', 11199,
+	N'cái', 1008, 'NK', N'328 lít, BH 3 năm')
+
+SELECT TOP 10 * 
+FROM HangHoa
+ORDER BY NgaySX DESC
+
+-- Giảm giá 5% cho mặt hàng có giá > 500$ 
+-- và sản xuất cách đây hơn 5 năm
+UPDATE HangHoa
+SET DonGia = DonGia * 0.95
+WHERE DonGia > 500 
+	AND (YEAR(GETDATE()) - YEAR(NgaySX) >=  5)
+
+--Thống kê hàng hóa
+SELECT MIN(DonGia) as GiaNhoNhat, SUM(SoLanXem),
+	AVG(DonGia) as GiaTrungBinh,
+	COUNT(MaHH) as SoLuong
+FROM HangHoa
+
+--Thống kê hàng hóa theo loại
+SELECT TenLoai, MIN(DonGia) as GiaNhoNhat, SUM(SoLanXem),
+	AVG(DonGia) as GiaTrungBinh,
+	COUNT(MaHH) as SoLuong
+FROM HangHoa as hh JOIN Loai as lo 
+	ON hh.MaLoai = lo.MaLoai
+GROUP BY lo.MaLoai, TenLoai
+HAVING AVG(DonGia) >= 30
+
+---------------VIEW
+CREATE VIEW vHangHoa AS
+SELECT MaHH, TenHH, DonGia, lo.MaLoai, TenLoai,
+	ncc.MaNCC, ncc.TenCongTy as NhaCungCap
+FROM HangHoa hh JOIN Loai lo ON lo.MaLoai = hh.MaLoai
+	JOIN NhaCungCap ncc ON ncc.MaNCC = hh.MaNCC
+
+SELECT * FROM vHangHoa 
+WHERE DonGia BETWEEN 100 AND 999
+
+--Tạo view lấy thông hóa đơn
+CREATE VIEW vHoaDon AS
+SELECT cthd.MaHH, TenHH, cthd.DonGia, cthd.SoLuong,
+	cthd.MaHD, NgayDat, MaKH, TenLoai
+FROM HoaDon hd JOIN ChiTietHD cthd ON cthd.MaHD = hd.MaHD
+	JOIN HangHoa hh ON hh.MaHH = cthd.MaHH
+	JOIN Loai lo on lo.MaLoai = hh.MaLoai
+
+SELECT * FROM vHoaDon WHERE MaHD = 10248
+
+------STORE PROCEDURE
+-- Thêm loại
+CREATE PROC spThemLoai
+	@MaLoai int output,
+	@TenLoai nvarchar(50),
+	@MoTa nvarchar(max),
+	@Hinh nvarchar(50)
+AS BEGIN
+	INSERT INTO Loai(TenLoai, MoTa, Hinh)
+		VALUES(@TenLoai, @MoTa, @Hinh)
+	
+	SET @MaLoai = @@IDENTITY
+END
+
+--DEMO
+DECLARE @Loai int
+exec spThemLoai @Loai output, N'Nước giải khát', NULL, NULL
+PRINT CONCAT(N'Loại vừa thêm có mã: ', @Loai)
+
+CREATE PROC spSuaLoai 
+	@MaLoai int,
+	@TenLoai nvarchar(50),
+	@MoTa nvarchar(max),
+	@Hinh nvarchar(50)
+AS BEGIN
+	UPDATE Loai SET TenLoai = @TenLoai, MoTa = @MoTa,
+		Hinh=@Hinh WHERE MaLoai = @MaLoai
+END
+
+exec spSuaLoai 1011, N'Bia', N'Bia đủ loại', NULL
---
+++ SQLScript_Day12.sql
@@ -0,0 +1,68 @@
+﻿--- DEMO FUNCTION
+--Function tính doanh thu theo khách hàng
+CREATE FUNCTION DoanhThuTheoKhachHang
+(
+	@MaKH nvarchar(50),
+	@Nam int
+)
+RETURNS float
+AS BEGIN
+	--B1: Khai báo
+	DECLARE @DoanhSo float
+	--B2: Tính toán
+	SELECT @DoanhSo = SUM(cthd.DonGia * cthd.SoLuong * 
+		(1 - cthd.GiamGia))
+	FROM ChiTietHD cthd JOIN HoaDon hd ON hd.MaHD = cthd.MaHD
+	WHERE hd.MaKH = @MaKH AND YEAR(hd.NgayDat) = @Nam
+	--B3: Trả về
+	RETURN @DoanhSo
+END
+
+SELECT dbo.DoanhThuTheoKhachHang('ANTON', 1996)
+
+SELECT MaKH, HoTen, 
+	dbo.DoanhThuTheoKhachHang(MaKH, 1996) as DoanhThu
+FROM KhachHang
+WHERE dbo.DoanhThuTheoKhachHang(MaKH, 1996) > 0
+ORDER BY DoanhThu DESC
+
+
+--- FUNCTION trả về table (có 2 cách viết)
+--Thống kê doanh thu khách hàng theo năm
+CREATE FUNCTION ThongKeDoanhThuTheoNam
+(
+	@Nam int
+)
+RETURNS TABLE AS RETURN
+	SELECT hd.MaKH, kh.HoTen,
+		SUM(cthd.DonGia * cthd.SoLuong * (1 - cthd.GiamGia))
+		as DoanhThu
+	FROM ChiTietHD cthd JOIN HoaDon hd ON hd.MaHD = cthd.MaHD
+		JOIN KhachHang kh ON kh.MaKH = hd.MaKH
+	WHERE YEAR(NgayDat) = @Nam
+	GROUP BY hd.MaKH, kh.HoTen
+
+SELECT * FROM dbo.ThongKeDoanhThuTheoNam(1996)
+
+-----Tạo function trả về bảng - cách đầy đủ
+-----Thống kê doanh thu theo loại, nhà cung cấp
+CREATE FUNCTION ThongKeDoanhThuHangHoaTheoNam
+(	@Nam int	)
+RETURNS @tmp TABLE (
+	TenLoai nvarchar(50), 
+	NhaCungCap nvarchar(50),
+	DoanhThu float
+)
+AS BEGIN
+	INSERT INTO @tmp SELECT TenLoai, TenCongTy,
+		SUM(cthd.DonGia * cthd.SoLuong * (1 - cthd.GiamGia))
+	FROM ChiTietHD cthd JOIN HoaDon hd ON hd.MaHD = cthd.MaHD
+		JOIN HangHoa hh ON hh.MaHH = cthd.MaHH
+		JOIN Loai lo ON lo.MaLoai = hh.MaLoai
+		JOIN NhaCungCap ncc ON ncc.MaNCC = hh.MaNCC
+	WHERE YEAR(NgayDat) = @Nam
+	GROUP BY TenLoai, TenCongTy
+	RETURN
+END
+
+SELECT * FROM dbo.ThongKeDoanhThuHangHoaTheoNam(1996)
