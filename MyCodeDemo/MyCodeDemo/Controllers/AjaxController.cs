dc29f60de853fedbe558878302504dee728291a5
Hien Luong <hyhien@gmail.com>
2021-12-04 15:14:28 +0700

    Demo AJax

---
+++ MyCodeDemo/MyCodeDemo/Controllers/AjaxController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCodeDemo.Entities;
+using MyCodeDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCodeDemo.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly eStore20Context _context;
+
+        public AjaxController(eStore20Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetServerTime()
+        {
+            return DateTime.Now.ToString("dd/MM/yyy hh:mm:sss");
+        }
+
+
+        #region Search
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProcessSearch(string keyword)
+        {
+            var result = _context.HangHoa.AsQueryable();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                result = result.Where(hh => hh.TenHh.Contains(keyword));
+            }
+            var data = result.Select(hh => new HangHoaViewModel { 
+                TenHh = hh.TenHh,
+                MaHh = hh.MaHh,
+                DonGia = hh.DonGia.Value,
+                Hinh = hh.Hinh
+            });
+
+            return PartialView(data.ToList());
+        }
+        #endregion
+    }
+}
---
+++ MyCodeDemo/MyCodeDemo/ViewModels/HangHoaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCodeDemo.ViewModels
+{
+    public class HangHoaViewModel
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+    }
+}
