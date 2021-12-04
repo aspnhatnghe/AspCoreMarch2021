2f6c0830fd5c4aeba3c8118be442dd2b107e8b8e
Hien Luong <hyhien@gmail.com>
2021-12-04 16:08:32 +0700

    Ajax json

--- MyCodeDemo/MyCodeDemo/Controllers/AjaxController.cs
+++ MyCodeDemo/MyCodeDemo/Controllers/AjaxController.cs
@@ -60,5 +60,39 @@ namespace MyCodeDemo.Controllers
             return PartialView(data.ToList());
         }
         #endregion
+
+        #region AjaxReturnJson
+        public IActionResult DemoAjaxJson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProjectSearchJson(string keyword, double? from, double? to)
+        {
+            var result = _context.HangHoa.AsQueryable();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                result = result.Where(hh => hh.TenHh.Contains(keyword));
+            }
+            if (from.HasValue)
+            {
+                result = result.Where(hh => hh.DonGia >= from.Value);
+            }
+            if (to.HasValue)
+            {
+                result = result.Where(hh => hh.DonGia <= to.Value);
+            }
+            var data = result.Select(hh => new HangHoaViewModel
+            {
+                TenHh = hh.TenHh,
+                MaHh = hh.MaHh,
+                DonGia = hh.DonGia.Value,
+                Hinh = hh.Hinh
+            });
+
+            return Json(data.ToList());
+        }
+        #endregion
     }
 }
