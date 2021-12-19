0333d8c68c75a919857dfca039ef41a23e12bb33
Hien Luong <hyhien@gmail.com>
2021-12-19 09:52:10 +0700

    Add ViewComponent CategoryMenu

---
+++ FinalProject/FinalProject/ViewComponents/CategoryMenu.cs
@@ -0,0 +1,27 @@
+﻿using FinalProject.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.ViewComponents
+{
+public class CategoryMenu : ViewComponent
+    {
+        private readonly NhatNgheDbContext _context;
+
+        public CategoryMenu(NhatNgheDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task    <IViewComponentResult> InvokeAsync()
+        {
+            var data = await _context.Categories.ToListAsync();
+
+            return View(data);
+        }
+    }
+}
