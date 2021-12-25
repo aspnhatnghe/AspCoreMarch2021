4a01cb70ffbb8b90bccc3a3afda9bd05e908f5a2
Hien Luong <hyhien@gmail.com>
2021-12-25 16:51:42 +0700

    Add to cart

---
+++ FinalProject/FinalProject/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(Guid id, int qty = 1)
+        {
+            try
+            {
+                return Json(new { Success = true, Total = 1, Count = 3 });
+            }
+            catch
+            {
+                return Json(new
+                {
+                    Success = false
+                });
+            }
+        }
+    }
+}
---
+++ FinalProject/FinalProject/ViewModels/CartItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.ViewModels
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public string SeoUrl { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double Total => Price * Quantity;
+    }
+}
